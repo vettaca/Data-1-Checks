6166a8ac3abec120e50bf7a5e34bacee804221a6
Ivette Lindsay <shawneeville@gmail.com>
2022-09-27 16:23:20 -0400

    Added First Knowledge Check

---
+++ Knowledge_Check_1.py
@@ -0,0 +1,19 @@
+#Step 1 print 'hello world'
+print('hello world') 
+
+#Step 2 print a specific item from a list
+thefruits = ["peaches", "plums", "tomatoes"]
+print(thefruits[0]) 
+
+#Step 3 create a dictionary 
+royalbadness = {
+  "musician": "Prince",
+  "Albums" : ["PurpleRain","Parade","DiamondsandPearls"],
+  "years" : [1984, 1986, 1991]
+  
+}
+print(royalbadness["musician"])
+
+#Step 4 create a tuple 
+oldschoolmc = ("kurtis blow", "llcoolj", "DOC")
+print(oldschoolmc[2])
